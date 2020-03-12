1bb4fa80f50f0bed24f81cf574e5da414d84f7fc
Michael Bykovski <michael.bykovskii@aoe.com>
2020-03-12 12:04:48 +0100

    DIGIHUB-0 add cors to allow any origins

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from interface.routers import users, generators, game, upgrades
 
@@ -8,6 +9,13 @@ app.include_router(users.router, prefix='/users', tags=['Users'])
 app.include_router(generators.router, prefix='/generators', tags=['Generators'])
 app.include_router(upgrades.router, prefix='/upgrades', tags=['Upgrades'])
 app.include_router(game.router, prefix='/game', tags=['Game'])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
