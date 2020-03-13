efd1b2acf0802b621b0e328f98a32f6b9fc55718
Michael Bykovski <michael.bykovskii@aoe.com>
2020-03-13 13:20:26 +0100

    Add available generators endpoint

--- interface/routers/generators.py
+++ interface/routers/generators.py
@@ -3,26 +3,27 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
+from interface.controller.available_generators_controller import AvailableGeneratorsController
 from interface.controller.purchase_controller import PurchaseController
 from interface.exceptions.not_sufficient_points import NotSufficientPoints
-from interface.models.generators import GeneratorPurchase
-from interface.utils import oauth2_scheme, get_current_user, get_db
+from interface.models.generators import GeneratorPurchase as GeneratorPurchaseModel, Generator as GeneratorModel
+from interface.utils import get_current_user, get_db
 from models import User, Generator
 
 router = APIRouter()
 
 
-@router.get('/available')
-async def available_generators(token: str = Depends(oauth2_scheme)):
-    return {'message': 'token'}
+@router.get('/available', response_model=List[GeneratorModel])
+async def available_generators(user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    return AvailableGeneratorsController(db).available_generators(user)
 
 
-@router.get('/current-user', response_model=List[GeneratorPurchase])
+@router.get('/current-user', response_model=List[GeneratorPurchaseModel])
 async def get_current_users_generators(user: User = Depends(get_current_user)):
     return user.generator_purchases
 
 
-@router.post('/{generator_id}/buy', response_model=GeneratorPurchase)
+@router.post('/{generator_id}/buy', response_model=GeneratorPurchaseModel)
 async def buy_generator(generator_id: int, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     generator = db.query(Generator).filter(Generator.id == generator_id).first()
     if generator is None:
