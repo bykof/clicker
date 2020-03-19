7255fb9f3d8850fdba9c4fc6d81ec3f325abe446
Michael Bykovski <michael.bykovskii@aoe.com>
2020-03-19 18:41:55 +0100

    Make access_token_expire_hours and generator_multiplier accessable by env vars

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
 import os
 
-GENERATOR_MULTIPLIER = 1.15
+GENERATOR_MULTIPLIER = float(os.environ.get('GENERATOR_MULTIPLIER', 1.15))
 SECRET_KEY = os.environ.get('SECRET_KEY', '123')
 JWT_ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_HOURS = 24
+ACCESS_TOKEN_EXPIRE_HOURS = int(os.environ.get('ACCESS_TOKEN_EXPIRE_HOURS', 24))
 REDIS_URL = os.environ.get('REDIS_URL', 'localhost')
 POSTGRES_URL = os.environ.get('POSTGRES_URL', 'postgres://localhost/clicker')
